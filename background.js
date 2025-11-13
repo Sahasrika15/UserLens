5de3c7d9b733b1bfc957f3393be34e7434871d1e
Karthikeya0201 <kaspakarthikeya02@gmail.com>
2025-11-13 21:53:50 +0530

    first commit in dev.

--- background.js
+++ background.js
@@ -3,20 +3,3 @@ chrome.runtime.onInstalled.addListener((details) => {
     chrome.tabs.create({ url: chrome.runtime.getURL("setup.html") });
   }
 });
-chrome.storage.sync.get(['finger_enabled'], res => {
-  const enabled = res.finger_enabled !== false;
-  chrome.declarativeContent?.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-      actions: [new chrome.declarativeContent.RequestContentScript({
-        js: enabled ? ['finger-control.js'] : []
-      })]
-    }]);
-  });
-});
-
-chrome.storage.onChanged.addListener((changes) => {
-  if (changes.finger_enabled) {
-    chrome.tabs.reload();
-  }
-});
